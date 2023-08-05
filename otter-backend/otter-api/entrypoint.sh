5ee77e8c0d309a4fd3c8746556c9764f9f2429f1
Syarwina <syarwinaaa09@gmail.com>
2023-08-05 20:36:10 +0800

    pipelines for encryption & decryption

--- otter-backend/otter-api/entrypoint.sh
+++ otter-backend/otter-api/entrypoint.sh
@@ -2,4 +2,4 @@
 
 RUN_PORT=${PORT:-8000}
 
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${PORT}"
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${PORT}" app.main:app
