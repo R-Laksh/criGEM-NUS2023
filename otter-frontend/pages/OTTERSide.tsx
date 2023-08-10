0f2ef80cdbe7b9e9d583eda0f574ff190804dba4
Syarwina <syarwinaaa09@gmail.com>
2023-08-10 10:56:47 +0800

    implementing nextauth

--- otter-frontend/pages/OTTERSide.tsx
+++ otter-frontend/pages/OTTERSide.tsx
@@ -1,21 +1,14 @@
 import type { NextPage } from 'next';
 import Head from "next/head";
-import Image from "next/image"
 import Header from "../components/Header2"
 import '../styles/globals.css';
-import { SessionProvider } from "../components/SessionProvider";
-import { getServerSession } from "next-auth";
-import type { Metadata } from 'next';
-import { authOptions } from "./api/auth/[...nextauth]";
-import Login from "../components/Login";
-import ClientProvider from "../components/ClientProvider";
 
 const OTTERLand: NextPage = () => {
 
   return (
     <div className="">
       <Head>
-        <title>OTTERLand</title>
+        <title>OTTERSIDE</title>
       </Head>
       <Header />
     </div>
