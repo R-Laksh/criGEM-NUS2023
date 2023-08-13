4d5e0cbbfd8252aa472247bd573a4db6b6a46cff
Syarwina <syarwinaaa09@gmail.com>
2023-08-13 18:01:47 +0800

    building the body section of the postbox

--- otter-frontend/components/PostBox.tsx
+++ otter-frontend/components/PostBox.tsx
@@ -1,60 +1,79 @@
 import { useSession } from 'next-auth/react';
-import React from 'react'
+import React, { useState } from 'react';
 import Avatar from "./Avatar";
 import { LinkIcon, PhotoIcon } from "@heroicons/react/24/outline";
 import { useForm } from "react-hook-form";
 
 type FormData = {
-    postTitle: string
-    postBody: string
-    postImage: string
-    space: string
-}
+    postTitle: string;
+    postBody: string;
+    postImage: string;
+    space: string;
+};
+
 function PostBox() {
     const { data: session } = useSession();
+    const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false);
     const {
         register,
         setValue,
         handleSubmit,
         watch,
         formState: { errors },
-    } = useForm<FormData>()
+    } = useForm<FormData>();
 
-  return <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white
-        p-2">
-        <div className="flex items-center space-x-3">
-            <Avatar />
+    return (
+        <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
+            <div className="flex items-center space-x-3">
+                <Avatar />
 
-            <input
-                {...register('postTitle', { required: true })}
-                disabled={!session} 
-                className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
-                type="text" 
-                placeholder={ 
-                    session ? "Create a post by entering a title!" : 'Sign in to post'
-                } 
-            />
+                <input
+                    {...register('postTitle', { required: true })}
+                    disabled={!session} 
+                    className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
+                    type="text" 
+                    placeholder={session ? "Create a post by entering a title!" : 'Sign in to post'}
+                />
 
-            <PhotoIcon className={`h-6 cursor-pointer text-gray-300 `}/>
-            <LinkIcon className="h-6 text-gray-300"/>
-        </div>
-
-        {!!watch('postTitle') && (
-            <div className="flex flex-col py-2">
-                {/* Body */}
-                <div className="flex items-center px-2">
-                    <p className="min-w-[90px]">Body:</p>
-                    <input 
-                        className="m-2 flex-1 bg-blue-50 p-2 outline-none"
-                        {...register('postBody')} type="text" placeholder="Text (optional)" />
-                </div>
-
-                <div>
-                    <p></p>
-                </div>
+                <PhotoIcon onClick={() => setImageBoxOpen(!imageBoxOpen)} className={`h-6 cursor-pointer text-gray-300 ${imageBoxOpen && 'text-blue-300'}`} />
+                <LinkIcon className="h-6 text-gray-300"/>
             </div>
-        )}
-    </form>
+
+            {watch('postTitle') && (
+                <div className="flex flex-col py-2">
+                    {/* Body */}
+                    <div className="flex items-center px-2">
+                        <p className="min-w-[90px]">Body:</p>
+                        <input 
+                            className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                            {...register('postBody')} type="text" placeholder="Text (optional)" />
+                    </div>
+
+                    <div className="flex items-center px-2"> 
+                        <p className="min-w-[90px]">Space:</p>
+                        <input
+                            className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                            {...register('space')}
+                            type="text"
+                            placeholder="i.e. Datasets"
+                        />
+                    </div>
+                </div>
+            )}
+
+            {imageBoxOpen && (
+                <div className="flex items-center px-2"> 
+                    <p className="min-w-[90px]">Image URL:</p>
+                    <input
+                        className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                        {...register('postImage')}
+                        type="text"
+                        placeholder="Optional..."
+                    />
+                </div>
+            )}
+        </form>
+    );
 }
 
 export default PostBox;
