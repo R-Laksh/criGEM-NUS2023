bab7e8a54070ee4c9d79704653f502b390e6c55b
Syarwina <syarwinaaa09@gmail.com>
2023-08-15 13:21:09 +0800

    building the votes section of post component

--- otter-frontend/components/Post.tsx
+++ otter-frontend/components/Post.tsx
@@ -15,13 +15,14 @@ type Props = {
 
 function Post({ post } : Props) {
   return (
-    <div>
+    <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border hover:border-gray-600">
         {/* Votes */}
-        <div>
-          <ArrowUpIcon />
-          <ArrowDownIcon />
+        <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
+          <ArrowUpIcon className="voteButtons hover:text-red-400"/>
+          <p className="text-xs font-bold text-black">0</p>
+          <ArrowDownIcon className="voteButtons hover:text-blue-400"/>
         </div>
-        <div>
+        <div className="p-3 pb-1">
             {/* Header */}
             <div>
 
