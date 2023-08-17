f8ca467efeebbc31fbe9f525ff5712de1abb359c
Syarwina <syarwinaaa09@gmail.com>
2023-08-17 12:08:46 +0800

    implementing the upvote & downvote functionality

--- otter-frontend/components/Post.tsx
+++ otter-frontend/components/Post.tsx
@@ -63,6 +63,22 @@ function Post({ post } : Props) {
     console.log('PLACED VOTE:', data)
   }
 
+  const displayVotes = (data: any)  => {
+    const votes: Vote[] = data?.getVotesByPostId
+    const displayNumber = votes?.reduce(
+      (total, vote) => (vote.upvote ? (total += 1 ) : (total -= 1)),
+      0
+    )
+
+    if (votes?.length === 0) return 0;
+
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1
+    }
+
+    return displayNumber;
+  }
+
   useEffect(() => {
     const votes: Vote[] = data?.getVotesByPostId
 
@@ -88,7 +104,7 @@ function Post({ post } : Props) {
         {/* Votes */}
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
           <ArrowUpIcon onClick={() => upVote(true)}className={`voteButtons hover:text-red-400 ${vote && 'text-red-400'}`}/>
-          <p className="text-xs font-bold text-black">0</p>
+          <p className="text-xs font-bold text-black">{displayVotes(data)}</p>
           <ArrowDownIcon onClick={() => upVote(false)} className={`voteButtons hover:text-blue-400 ${vote == false && 'text-blue-400'}`}/>
         </div>
         <div className="p-3 pb-1">
