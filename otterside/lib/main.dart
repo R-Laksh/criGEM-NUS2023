be4418b001cee8681e65deaed9bfd5bc7fb61db0
Syarwina <syarwinaaa09@gmail.com>
2023-08-22 14:02:50 +0800

    firebase setup

--- otterside/lib/main.dart
+++ otterside/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:otterside/features/auth/screens/login_screen.dart';
+import 'package:otterside/firebase_options.dart';
 import 'package:otterside/theme/palette.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
