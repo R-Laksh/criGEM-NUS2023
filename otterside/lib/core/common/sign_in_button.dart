64ea6ea95e87fc837cd26975c125e2ca207a9d00
Syarwina <syarwinaaa09@gmail.com>
2023-08-23 14:47:01 +0800

    storing data to provider

--- otterside/lib/core/common/sign_in_button.dart
+++ otterside/lib/core/common/sign_in_button.dart
@@ -7,15 +7,15 @@ import 'package:otterside/theme/palette.dart';
 class SignInButton extends ConsumerWidget {
   const SignInButton({ Key? key}) : super(key: key);
 
-  void signInWithGoogle(WidgetRef ref) {
-    ref.read(authControllerProvider).signInWithGoogle();
+  void signInWithGoogle(BuildContext context, WidgetRef ref) {
+    ref.read(authControllerProvider.notifier).signInWithGoogle(context);
   }
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-        onPressed: () => signInWithGoogle(ref),
+        onPressed: () => signInWithGoogle(context, ref),
         icon: Image.asset(
           Constants.googlePath, 
           width: 35, 
