ee73c36313865600f30131d49bfe4394ea7ad0f4
Syarwina <syarwinaaa09@gmail.com>
2023-08-23 14:54:03 +0800

    flutter route management

--- otterside/lib/main.dart
+++ otterside/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:otterside/features/auth/screens/login_screen.dart';
 import 'package:otterside/firebase_options.dart';
+import 'package:otterside/router.dart';
 import 'package:otterside/theme/palette.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:routemaster/routemaster.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp(
@@ -22,11 +24,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'OTTERside',
       theme: Pallete.darkModeAppTheme,
-      home: const LoginScreen(),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) => loggedOutRoute),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
