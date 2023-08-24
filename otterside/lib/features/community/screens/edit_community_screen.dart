bbfd6df105664510cba23bd28a9bb765ac75982a
Syarwina <syarwinaaa09@gmail.com>
2023-08-24 22:50:37 +0800

    editing community screen ui

--- otterside/lib/features/community/screens/edit_community_screen.dart
+++ otterside/lib/features/community/screens/edit_community_screen.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:otterside/core/common/error_text.dart';
+import 'package:otterside/core/common/loader.dart';
+import 'package:otterside/core/constants/constants.dart';
+import 'package:otterside/core/utils.dart';
+import 'package:otterside/features/community/controller/community_controller.dart';
+import 'package:otterside/theme/palette.dart';
 
 class EditCommunityScreen extends ConsumerStatefulWidget {
   final String name;
@@ -13,8 +21,97 @@ class EditCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
+  File? bannerFile;
+  File? profileFile;
+  
+  void selectBannerImage() async {
+    final res = await pickImage();
+
+    if (res!=null) {
+      setState(() {
+        bannerFile = File(res.files.first.path!);
+      });
+    }
+  }
+
+  void selectProfileImage() async {
+    final res = await pickImage();
+
+    if (res != null) {
+      setState(() {
+        profileFile = File(res.files.first.path!);
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ref.watch(getCommunityByNameProvider(widget.name)).when(
+      data: (community) => Scaffold(
+        backgroundColor: Pallete.darkModeAppTheme.backgroundColor,
+        appBar: AppBar(
+          title: const Text('Edit Space'),
+          centerTitle: false,
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                child: Stack(
+                  children: [
+                    GestureDetector(
+                      onTap: selectBannerImage,
+                      child: DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 4],
+                          strokeCap: StrokeCap.round,
+                          color: Pallete.darkModeAppTheme.textTheme.bodyText2!.color!,
+                          child: Container(
+                          width: double.infinity,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: bannerFile!=null 
+                          ? Image.file(bannerFile!)
+                          : community.banner.isEmpty || community.banner == Constants.bannerDefault
+                          ? const Center(
+                            child: Icon(
+                              Icons.camera_alt_outlined, 
+                              size: 40, 
+                              ),
+                            )
+                          : Image.network(community.banner),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      left: 20,
+                      child: GestureDetector(
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(community.avatar),
+                          radius: 32,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      loading: () => const Loader(),
+      error: (error, stackTrace) => ErrorText(error: error.toString()),
+    );
   }
 }
