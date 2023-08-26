f4376545fedd0864b7670e8d98ea35db89311200
Syarwina <syarwinaaa09@gmail.com>
2023-08-26 12:48:02 +0800

    adding moderators

--- otterside/lib/router.dart
+++ otterside/lib/router.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:otterside/features/auth/screens/login_screen.dart';
+import 'package:otterside/features/community/screens/add_mods_screen.dart';
 import 'package:otterside/features/community/screens/community_screen.dart';
 import 'package:otterside/features/community/screens/create_community_screen.dart';
 import 'package:otterside/features/community/screens/edit_community_screen.dart';
@@ -29,6 +30,11 @@ final loggedInRoute = RouteMap(routes: {
   ),
   '/edit-community/:name': (routeData) => MaterialPage(
       child: EditCommunityScreen(
+    name: routeData.pathParameters['name']!,
+    ),
+  ),
+  '/add-mods/:name': (routeData) => MaterialPage(
+      child: AddModsScreen(
     name: routeData.pathParameters['name']!,
     ),
   ),
