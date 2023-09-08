d545217b8fb35ba4c55eb16673b217df6e0f89b2
Syarwina <syarwinaaa09@gmail.com>
2023-09-08 19:28:54 +0800

    commit

--- otter-frontend/lib/queryApi.ts
+++ otter-frontend/lib/queryApi.ts
@@ -22,7 +22,7 @@ const query = async (prompt: string, chatId: string, model: string) => {
         randomNumber.toFixed(5)
     );
 
-    return `The binding score of this RNA is ${res}. More results generation soon.`;
+    return `The binding score of this RNA is ${res}.`;
 };
 
 export default query;
